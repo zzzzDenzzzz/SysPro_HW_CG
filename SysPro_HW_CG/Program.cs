2fd965624836156f9c236112f1c77774c3a29e29
zzzzDenzzzz <12den24@gmail.com>
2023-03-18 11:33:50 +0300

    Добавьте файлы проекта.

---
+++ SysPro_HW_CG/Program.cs
@@ -0,0 +1,18 @@
+﻿using SysPro_HW_CG;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        using (TestClass test = new())
+        {
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine("Что-то происходит");
+        }
+
+        Console.ForegroundColor = ConsoleColor.DarkBlue;
+        Console.WriteLine("Опять что-то происходит");
+
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+}
---
+++ SysPro_HW_CG/TestClass.cs
@@ -0,0 +1,16 @@
+﻿namespace SysPro_HW_CG
+{
+    internal class TestClass : IDisposable
+    {
+        public TestClass()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("I am an object of class TestClass");
+        }
+        public void Dispose()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("GC dispose object of class TestClass");
+        }
+    }
+}
